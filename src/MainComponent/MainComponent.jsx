b1d48af01b6634a522e37bf86ae82523f39dde07
iGorillaz <barannik.denis.92@gmail.com>
2022-07-08 15:46:58 +0300

    Project init

---
+++ src/MainComponent/MainComponent.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const MainComponent = () => {
+	const [input, setInput] = useState('');
+	const [seconds, setSeconds] = useState(0);
+
+
+  return (<div>Hello World!</div>);
+};
+
+export default MainComponent;

de46c70ff47f304f9f1c1ca78c6b23e598d17375
iGorillaz <barannik.denis.92@gmail.com>
2022-07-08 17:07:12 +0300

    useRef logic  added

--- src/MainComponent/MainComponent.jsx
+++ src/MainComponent/MainComponent.jsx
@@ -1,18 +1,51 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const MainComponent = () => {
   const [input, setInput] = useState("");
   const [seconds, setSeconds] = useState(0);
 
+  const renders = useRef(0);
+  const inputRef = useRef(0);
+  const timerId = useRef(0);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    renders.current++;
+  };
+
+  const startTimer = () => {
+    timerId.current = setInterval(() => {
+      renders.current++;
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+    inputRef.current.focus();
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerId.current);
+    timerId.current = 0;
+    inputRef.current.focus();
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    if (seconds) {
+      renders.current++;
+      setSeconds(0);
+    }
+    inputRef.current.focus();
+  };
+
   return (
     <div>
       <input
+        ref={inputRef}
         type="text"
         value={input}
         placeholder="Input text"
         onChange={handleChange}
       />
-      <p>Renders: </p>
+      <p>Renders: {renders.current} </p>
       <br />
       <br />
       <section>
